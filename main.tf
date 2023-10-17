7c2dc3351fef8d478b0ee67c5dafdc7daffb57cf
Dominik Schorn <dominik.schorn@nordlb.de>
2023-10-17 13:55:04 +0200

    test

--- main.tf
+++ main.tf
@@ -1,40 +1,13 @@
-resource "aws_security_group" "allow_tls" {
-  name        = "allow_tls"
-  description = "Allow TLS inbound traffic"
-  vpc_id      = aws_vpc.main.id
-
-  ingress {
-    description      = "TLS from VPC"
-    from_port        = 443
-    to_port          = 443
-    protocol         = "tcp"
-    cidr_blocks      = [aws_vpc.main.cidr_block]
-    ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
-  }
-
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = "-1"
-    cidr_blocks      = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-  }
-
-  tags = {
-    Name = "allow_tls"
-  }
+locals {
+  test = true
 }
 
-resource "azurerm_storage_account" "this" {
-  name                            = "ststore"
-  resource_group_name             = azurerm_resource_group.this.name
-  location                        = local.cluster.azure_location
-  account_tier                    = "Standard"
-  account_replication_type        = "LRS"
-  allow_nested_items_to_be_public = false
-  min_tls_version                 = "TLS1_2"
+resource "random_pet" "this" {
+  count = local.test ? 1 : 0
 }
 
-variable "test" {
-  type = any
+resource "null_resource" "this" {
+  provisioner "local-exec" {
+    command = "echo '192.168.0.1'"
+  }
 }
