2b3410b7a8921d08339b9c5f9af7dcce5066fdcc
Dominik Schorn <dominik.schorn@nordlb.de>
2023-12-11 08:55:44 +0100

    test: upload plan

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ locals {
 }
 
 resource "random_pet" "this" {
-  count = local.test ? 1 : 0
+  count = local.test ? 10 : 0
 }
 
 resource "null_resource" "this" {
@@ -11,3 +11,13 @@ resource "null_resource" "this" {
     command = "echo '192.168.0.1'"
   }
 }
+
+resource "azurerm_storage_account" "this" {
+  name                            = "this"
+  resource_group_name             = "this"
+  location                        = "this"
+  account_tier                    = "Standard"
+  account_replication_type        = "LRS"
+  allow_nested_items_to_be_public = true
+}
+
